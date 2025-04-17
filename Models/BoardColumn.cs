4097003f4d3664b738b757d89cafb6f06cae5699
YashikaAgg <47354757+YashikaAgg@users.noreply.github.com>
2025-04-17 12:45:25 +0530

    Add files via upload

---
+++ Models/BoardColumn.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TaskManagementApp.Shared.Enum;
+
+namespace TaskManagementApp.Models
+{
+    public class BoardColumn
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public BoardColumnType ColumnType { get; set; }
+    }
+}
---
+++ Models/TaskItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementApp.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public int BoardColumnId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime? Deadline { get; set; }
+        public bool IsFavorite { get; set; }
+        // URL for an attached image (optional)
+        public string ImageUrl { get; set; }
+        public virtual BoardColumn BoardColumn { get; set; }
+    }
+}
